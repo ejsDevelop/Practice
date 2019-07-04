d7d360d72c358dd735a74ef522e9a5371b5a1681
Erik Schjolberg <erik.schjolberg@gmail.com>
2019-07-04 08:55:46 -0600

    commit

---
+++ all-code/javascript/stringInterpAdvanced.js
@@ -0,0 +1,32 @@
+
+/*
+Practice
+
+String interpolation advanced
+Complete the function getFullName such that it returns the full name of the person using interpolation.
+
+1234567891011
+/**
+ * @param {string} firstName
+ * @param {string} lastName
+ */
+function getFullName(firstName, lastName){
+  return `${firstName} ${lastName}`
+}
+
+//sample usage
+console.log(getFullName("John", "Doe"));
+
+
+Solution Run
+
+Tests (3 / 3)
+
+1 returns a string
+2 returns full name
+3 uses interpolation
+
+
+
+
+
